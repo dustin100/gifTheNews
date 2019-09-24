7ecccfd2eb638aff3a2b6cb086d06377877a9d83
Dustin <dmkelly100@gmail.com>
2019-09-24 19:07:33 -0400

    Updates

---
+++ precache-manifest.dc5824c9dbdcab29cdb0ea7569df6b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f971a502a0ca19534b7811c8e4472",
+    "url": "/gifTheNews/index.html"
+  },
+  {
+    "revision": "7781267a17cd9b922fd4",
+    "url": "/gifTheNews/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "49ae77e1252a75735c74",
+    "url": "/gifTheNews/static/css/main.86943671.chunk.css"
+  },
+  {
+    "revision": "7781267a17cd9b922fd4",
+    "url": "/gifTheNews/static/js/2.a46c4f6f.chunk.js"
+  },
+  {
+    "revision": "49ae77e1252a75735c74",
+    "url": "/gifTheNews/static/js/main.3378f14d.chunk.js"
+  },
+  {
+    "revision": "ff90ef7167c06cd84f12",
+    "url": "/gifTheNews/static/js/runtime~main.2cd2d9cc.js"
+  }
+]);
