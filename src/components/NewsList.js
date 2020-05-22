68953a10d3130aa18b662cdc3e68f47dc10026ea
Dustin <dmkelly100@gmail.com>
2020-05-21 20:42:17 -0400

    changed json file to work with netlify

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,118 +1,113 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://proxy.hackeryou.com';
 
 class NewsList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            news: [],
-            gifs: []
+	constructor(props) {
+		super(props);
+		this.state = {
+			news: [],
+			gifs: [],
+		};
+	}
 
-        }
-    }
+	renderContent() {
+		// const { news, gifs } = this.state; I can use to replace this.state
+		const articleList = this.state.news.map((items, index) => {
+			const gifToUse = this.state.gifs[index];
 
+			return (
+				<div key={gifToUse.id} className="boxWarpper">
+					{' '}
+					<a href={items.url} target="_blank" rel="noopener noreferrer">
+						<div className="mainImage">
+							<img src={gifToUse.images.original.url} alt={gifToUse.title} />{' '}
+						</div>
 
+						<h3 className="mainTitle">{items.title}</h3>
+						<p className="articleSource">
+							News Source: <span>{items.source.name}</span>
+						</p>
+						<p className="=desc"> {items.description}</p>
+					</a>
+				</div>
+			);
+		});
 
-    renderContent() {
-        // const { news, gifs } = this.state; I can use to replace this.state
-        const articleList = this.state.news.map((items, index) => {
-            const gifToUse = this.state.gifs[index];
+		return <div className="mainContent">{articleList}</div>;
+	}
 
-            return (
+	loadingState() {
+		return (
+			<div className="loadingContainer">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		);
+	}
 
+	async fetchNews() {
+		try {
+			const newsData = await axios.get('https://newsapi.org/v2/everything', {
+				params: {
+					apiKey: '0cc17ab873b944d08f7a6b7c222b403c',
+					sortBy: 'relevancy',
+					pageSize: 18,
+					language: 'en',
+					q: this.props.inputValue ? this.props.inputValue : 'breaking news',
+					page: this.props.newsPageNum ? this.props.newsPageNum : 1,
+				},
+			});
 
-                <div key={gifToUse.id} className="boxWarpper"> <a href={items.url} target="_blank" rel="noopener noreferrer">
-                    <div className="mainImage"><img src={gifToUse.images.original.url} alt={gifToUse.title} /> </div>
+			const gifData = await axios.get('https://api.giphy.com/v1/gifs/search', {
+				params: {
+					api_key: 'fqGHJALbZ8kmKJ6L76bs9FJmTV6GxJ8t',
+					limit: 18,
+					q: this.props.inputValue ? this.props.inputValue : 'breaking news',
+					offset: this.props.gifPageNum ? this.props.gifPageNum : 0,
+				},
+			});
 
-                    <h3 className="mainTitle">{items.title}</h3>
-                    <p className="articleSource">News Source: <span>{items.source.name}</span></p>
-                    <p className="=desc"> {items.description}</p>
-                </a>
-                </div >
-            )
+			this.setState({
+				news: newsData.data.articles,
+				gifs: gifData.data.data,
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	componentDidMount() {
+		this.fetchNews();
+	}
 
-        })
+	componentDidUpdate(prevProps, prevState) {
+		if (
+			this.props.inputValue !== prevProps.inputValue ||
+			this.props.newsPageNum !== prevProps.newsPageNum
+		) {
+			this.setState(
+				{
+					news: [],
+					gifs: [],
+				},
+				this.fetchNews
+			);
+		}
+	}
 
-        return (
-            <div className="mainContent" >
-                {articleList}
-            </div>
-        )
-
-
-
-    }
-
-    loadingState() {
-        return (
-            <div className="loadingContainer">
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-        )
-    }
-
-    async fetchNews() {
-        try {
-            const newsData = await axios.get('https://newsapi.org/v2/everything', {
-                params: {
-                    apiKey: "0cc17ab873b944d08f7a6b7c222b403c",
-                    sortBy: 'relevancy',
-                    pageSize: 18,
-                    language: 'en',
-                    q: this.props.inputValue ? this.props.inputValue : 'breaking news',
-                    page: this.props.newsPageNum ? this.props.newsPageNum : 1
-
-                }
-            })
-            const gifData = await axios.get('https://api.giphy.com/v1/gifs/search', {
-                params: {
-                    api_key: 'fqGHJALbZ8kmKJ6L76bs9FJmTV6GxJ8t',
-                    limit: 18,
-                    q: this.props.inputValue ? this.props.inputValue : 'breaking news',
-                    offset: this.props.gifPageNum ? this.props.gifPageNum : 0
-                }
-            })
-
-            this.setState({
-                news: newsData.data.articles,
-                gifs: gifData.data.data,
-
-
-            })
-
-
-
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    componentDidMount() {
-        this.fetchNews()
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.inputValue !== prevProps.inputValue || this.props.newsPageNum !== prevProps.newsPageNum) {
-            this.setState({
-                news: [],
-                gifs: []
-            }, this.fetchNews);
-        }
-    }
-
-    render() {
-        return (
-            <section>
-
-                {this.state.news.length ? this.renderContent() : this.loadingState()}
-            </section>
-        )
-    }
+	render() {
+		return (
+			<section>
+				{this.state.news.length ? this.renderContent() : this.loadingState()}
+			</section>
+		);
+	}
 }
 
-export default NewsList
+export default NewsList;
